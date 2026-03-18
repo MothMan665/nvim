daabd4f783f49ad35550b8b85a315278720a688a
MothMan665 <george@uddersorchard.co.uk>
2026-03-18 00:41:34 +0000

    removed lazy-lock from git  added inline git diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- optional - Diff integration
+		"YouSame2/inlinediff-nvim", -- inline diff
 
 		-- Only one of these is needed.
 		"nvim-telescope/telescope.nvim", -- optional
